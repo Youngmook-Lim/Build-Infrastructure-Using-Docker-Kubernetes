2e46bad1ac0a36ed7fe12db48a8b5da4026b733b
joykst96 <joykst96@gmail.com>
2023-03-30 14:12:31 +0900

    fix: modify determine unusual failure method

---
+++ til/seongtae/fail_analysis.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+FILE=jenkins-error-log
+
+grep -E 'Connection refused|timed out|UnknownHostException' "$FILE" && {echo 'network error'; exit 1;}
+grep -E 'Permission denied|No space left on device|Too many open files|Out of memory' "$FILE" && {echo 'resource error'; exit 1;}
+grep -E 'JAVA_HOME not set' "$FILE" && {echo 'environment error'; exit 1;}
+grep -E 'Invalid argument|Invalid input' "$FILE" && {echo 'argument error'; exit 1;}
+exit 0;
